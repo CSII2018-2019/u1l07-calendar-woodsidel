132a5d7652cc23d0b189eedf0452b8c944b06f0b
woodsidel <woodsidel@LR064WA7-1718.ellis.int>
2018-10-22 19:54:17 -0400

    Finished Project

--- src/U1L07CalendarMath.java
+++ src/U1L07CalendarMath.java
@@ -1,8 +1,21 @@
-
+import javax.swing.JOptionPane;
 public class U1L07CalendarMath {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String[] daysOfWeek = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+		String[] months = {"January","February","March","April","May","June","July","August","September","October","November","December"};
+		int[] daysInMonths = {31,28,31,30,31,30,31,31,30,31,30,31};
+		int i = 0;
+		String numberDay = JOptionPane.showInputDialog("What day of the year is it? ex: 81 ");
+		int numDay = Integer.parseInt(numberDay);
+		int weekDayNum = numDay%7;
+		String weekDay = daysOfWeek[weekDayNum-1];
+		while(numDay > daysInMonths[i]) {
+			numDay -= daysInMonths[i];
+			i++;
+		}
+		JOptionPane.showMessageDialog(null, "The date is "+weekDay+", "+months[i]+" "+numDay+", 2018.");
+		
 
 	}
 
